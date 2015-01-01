5fe8d95307553eae00d2181c754efb27e05df269
Yuyuko <xxsyuyuko@gmail.com>
2015-01-02 01:38:45 +0800

    add block specified keywords

--- js/modifyForum.js
+++ js/modifyForum.js
@@ -1,17 +1,23 @@
 var table = document.getElementById("threadlisttableid");
-for(var id = 0; id < localStorage.count; id++) {
+for (var id = 0; id < localStorage.count; id++) {
     var item = JSON.parse(localStorage.getItem("BlackList" + id));
     for (var i = 0; i < table.rows.length; i++) {
         if (table.rows[i].getAttribute("class") == "ts")
             continue;
         if (item.type == "username") {
             var curUsername = table.rows[i].cells[2].childNodes[1].childNodes[1].innerHTML;
-            console.log(curUsername);
             var username = item.value;
             if (username == curUsername) {
-                //table.rows[i].parentNode.setAttribute("hidden", "true");
                 var x = table.rows[i].parentNode;
-                console.log(x);
+                x.remove();
+                i--;
+            }
+        }
+        if (item.type == "key") {
+            var threadTopic = table.rows[i].cells[1].childNodes[7].innerHTML;
+            threadTopic = threadTopic.toLowerCase();
+            if(threadTopic.indexOf(item.value) >= 0) {
+                var x = table.rows[i].parentNode;
                 x.remove();
                 i--;
             }
@@ -20,6 +26,6 @@ for(var id = 0; id < localStorage.count; id++) {
     localStorage.removeItem("BlackList" + id);
 }
 localStorage.removeItem("count");
-if(table.tBodies.length > 0 && table.tBodies[0].getAttribute("id") == "separatorline"){
+if (table.tBodies.length > 0 && table.tBodies[0].getAttribute("id") == "separatorline") {
     table.tBodies[0].remove();
 }
