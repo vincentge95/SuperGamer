3bad2f01cbeec23cdd394ab2098856d212745db6
Yuyuko <xxsyuyuko@gmail.com>
2015-01-03 17:56:11 +0800

    update css style

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (tab.url.indexOf("http://bbs.sgamer.com/forum-") == 0) {
-        if (changeInfo.status == "complete") {
+        if (changeInfo.status == "loading") {
             chrome.tabs.executeScript(tabId, {code: "localStorage.setItem('count', " + localStorage.count + ");"});
             for (var i = 0; i < localStorage.count; i++) {
                 var temp = localStorage.getItem("BlackList" + i);
@@ -10,7 +10,7 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
         }
     }
     if(tab.url.indexOf("http://bbs.sgamer.com/thread-") == 0) {
-        if (changeInfo.status == "complete") {
+        if (changeInfo.status == "loading") {
             chrome.tabs.executeScript(tabId, {code: "localStorage.setItem('count', " + localStorage.count + ");"});
             for (var i = 0; i < localStorage.count; i++) {
                 var temp = localStorage.getItem("BlackList" + i);
--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,4 @@
-document.write("<table id = 'BlackList' border = '1'>");
+document.write("<table id = 'BlackList' class = 'table table-bordered'>");
 document.write("<tr>");
 document.write("<td>关键字</td>");
 document.write("<td>类型</td>");
@@ -9,8 +9,14 @@ if (localStorage.count) {
         var info = JSON.parse(localStorage.getItem("BlackList" + i));
         document.write("<tr>");
         document.write("<td>" + info.value + "</td>");
+        if(info.type == "username") {
+            info.type = "用户名"
+        }
+        else {
+            info.type = "关键字";
+        }
         document.write("<td>" + info.type + "</td>");
-        document.write("<td><input type = 'button' value = '删除' class = 'deleteInfo'></td>");
+        document.write("<td><input type = 'button' value = '删除' class = 'deleteInfo btn btn-xs btn-danger'></td>");
         document.write("</tr>");
     }
 }
@@ -35,8 +41,13 @@ function addInfo(type) {
     localStorage.setItem("BlackList" + id, JSON.stringify(info));
     var row = document.getElementById("BlackList").insertRow(Number(id) + 1);
     row.insertCell(0).innerHTML = value;
-    row.insertCell(1).innerHTML = type;
-    row.insertCell(2).innerHTML = "<input type = 'button' value = '删除' class = 'deleteInfo'>";
+    if(type == "username") {
+        row.insertCell(1).innerHTML = "用户名";
+    }
+    else {
+        row.insertCell(1).innerHTML = "关键字";
+    }
+    row.insertCell(2).innerHTML = "<input type = 'button' value = '删除' class = 'deleteInfo btn btn-xs btn-danger'>";
 
 }
 function deleteInfo(r) {
