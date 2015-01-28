8a504bdbf3d86c56b02b21f44c4140b982c512c8
Yuyuko <xxsyuyuko@gmail.com>
2015-01-29 02:37:34 +0800

    fix extension id

--- background.js
+++ background.js
@@ -3,14 +3,15 @@
 
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
+        var extensionId = chrome.runtime.id;
         if( details.url == "http://bbs.sgamer.com/data/cache/common_smilies_var.js?bVd" ) {
-            return {redirectUrl: "chrome-extension://kekbjbpfgbhalenooagmiicnpkaocdcb/js/ExtraEmotionVar.js?bVd"};
+            return {redirectUrl: "chrome-extension://" + extensionId + "/js/ExtraEmotionVar.js?bVd"};
         }
         if( details.url == "http://bbs.sgamer.com/static/js/smilies.js?bVd") {
-            return {redirectUrl: "chrome-extension://kekbjbpfgbhalenooagmiicnpkaocdcb/js/ExtraEmotion.js?bVd"};
+            return {redirectUrl: "chrome-extension://" + extensionId + "/js/ExtraEmotion.js?bVd"};
         }
         if(details.url == "http://bbs.sgamer.com/static/js/bbcode.js?bVd") {
-            return {redirectUrl: "chrome-extension://kekbjbpfgbhalenooagmiicnpkaocdcb/js/bbcode.js?bVd"};
+            return {redirectUrl: "chrome-extension://" + extensionId + "/js/bbcode.js?bVd"};
         }
     },
     {urls: ["http://bbs.sgamer.com/data/cache/common_smilies_var.js?bVd", "http://bbs.sgamer.com/static/js/smilies.js?bVd", "http://bbs.sgamer.com/static/js/bbcode.js?bVd"]},
