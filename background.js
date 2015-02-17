86f3e4e6e7611c6fb742475e520c3ec528bd136c
Yuyuko <xxsyuyuko@gmail.com>
2015-02-17 20:43:55 +0800

    Add message listener to extension.

--- background.js
+++ background.js
@@ -1,8 +1,7 @@
-// Check url and show pageAction.
-// Redirect js of bbs.sgamer.com to ext's.
 
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
+        // Redirect js of bbs.sgamer.com to ext's.
         var extensionId = chrome.runtime.id;
         if(details.url.indexOf("http://bbs.sgamer.com/data/cache/common_smilies_var.js") == 0) {
             var param = "";
@@ -16,6 +15,7 @@ chrome.webRequest.onBeforeRequest.addListener(
             }
             return {redirectUrl: "chrome-extension://" + extensionId + "/js/ExtraEmotionVar.js" + param};
         }
+        // Redirect js of bbs.sgamer.com to ext's.
         if(details.url.indexOf("http://bbs.sgamer.com/static/js/smilies.js") == 0) {
             var param = "";
             for(var i = 0; i < details.url.length; i++) {
@@ -28,6 +28,7 @@ chrome.webRequest.onBeforeRequest.addListener(
             }
             return {redirectUrl: "chrome-extension://" + extensionId + "/js/ExtraEmotion.js" + param};
         }
+        // Redirect js of bbs.sgamer.com to ext's.
         if(details.url.indexOf("http://bbs.sgamer.com/static/js/bbcode.js") == 0) {
             var param = "";
             for(var i = 0; i < details.url.length; i++) {
@@ -45,7 +46,30 @@ chrome.webRequest.onBeforeRequest.addListener(
     ["blocking"]
 );
 
+// Return localStorage to the querying page.
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.method == "getLocalStorage")
+            sendResponse(localStorage);
+    }
+);
 
+/*
+chrome.webRequest.onBeforeSendHeaders.addListener(
+    function(details) {
+        for (var i = 0; i < details.requestHeaders.length; ++i) {
+            if (details.requestHeaders[i].name === 'Cookie') {
+                details.requestHeaders.splice(i, 1);
+                break;
+            }
+        }
+        return {requestHeaders: details.requestHeaders};
+    },
+    {urls: ["http://c1.web.yystatic.com/*"]},
+    ["blocking", "requestHeaders"]
+);
+*/
+// Show pageAction.
 function checkForValidUrl(tabId, changeInfo, tab) {
     if (tab.url.indexOf("http://bbs.sgamer.com/") == 0) {
         chrome.pageAction.show(tabId);
